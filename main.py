00ca4c6e15cc344b297fdb615d4c97767e43c754
anjor <anjor@umd.edu>
2024-01-03 14:48:45 +0000

    Model routing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import marvin
 import streamlit as st
 
 from quiz import generate_questions, check_answers
@@ -29,6 +30,7 @@ def run_quiz(category: str, number_of_questions: int):
         st.session_state.questions = []
 
     if st.button("Start game"):
+        print(f"Using model to generate questions: {marvin.settings.llm_model}")
         st.session_state.questions = generate_questions(category=category, number_of_questions=number_of_questions)
 
     questions_with_answers = {}
@@ -38,10 +40,25 @@ def run_quiz(category: str, number_of_questions: int):
 
     if st.session_state.questions:
         if st.button("Check answers"):
+            print(f"Using model to check answers: {marvin.settings.llm_model}")
             _check_answers(check_answers_fn=check_answers, questions_with_answers=questions_with_answers)
 
 
 if __name__ == "__main__":
     title()
+
+    if "model" not in st.session_state:
+        st.session_state.model = "openai/gpt-3.5-turbo"
+
+    st.sidebar.header("Advanced settings")
+    model = st.sidebar.radio("Model", ["gpt-3.5-turbo", "mistral-tiny"])
+
+    if model != st.session_state.model:
+        st.session_state.model = model
+        marvin.settings.llm_model = f"openai/{st.session_state.model}"
+
+        print(marvin.settings.llm_model)
+
     category, number_of_questions = quiz_params()
+
     run_quiz(category=category, number_of_questions=number_of_questions)
