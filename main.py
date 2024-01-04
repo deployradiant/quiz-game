598843dd8ab8849d81701dfc1c8620218bc0b1c7
anjor <anjor@umd.edu>
2024-01-04 10:46:59 +0000

    Instructor implementation

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import streamlit as st
+from openai import OpenAI
 
-from quiz import generate_questions_with_marvin, check_answers_with_marvin
-from utils import _check_answers
+from quiz import generate_questions
+from utils import check_answers
 
 
 def title():
     st.title("Simple Quiz")
     st.sidebar.header("Instructions")
 
-    st.sidebar.markdown("This is a simple quiz game. You can select the category and the number of questions for the quiz.")
+    st.sidebar.markdown(
+        "This is a simple quiz game. You can select the category and the number of questions for the quiz."
+    )
     st.sidebar.write('Click the "Start game" button to start the quiz.')
     st.sidebar.write("If you want to play again, click the button again.")
 
@@ -19,17 +22,24 @@ def quiz_params():
     with cols[0]:
         category = st.text_input("**Category**", key="category")
     with cols[1]:
-        number_of_questions = st.text_input("**Number of questions**", key="number_of_questions")
+        number_of_questions = st.text_input(
+            "**Number of questions**", key="number_of_questions"
+        )
 
     return category, number_of_questions
 
 
-def run_quiz(category: str, number_of_questions: int):
+def run_quiz(client: OpenAI, model: str, category: str, number_of_questions: int):
     if "questions" not in st.session_state:
         st.session_state.questions = []
 
     if st.button("Start game"):
-        st.session_state.questions = generate_questions_with_marvin(category=category, number_of_questions=number_of_questions)
+        st.session_state.questions = generate_questions(
+            client=client,
+            model=model,
+            category=category,
+            number_of_questions=number_of_questions,
+        )
 
     questions_with_answers = {}
     for question in st.session_state.questions:
@@ -38,11 +48,23 @@ def run_quiz(category: str, number_of_questions: int):
 
     if st.session_state.questions:
         if st.button("Check answers"):
-            results = check_answers_with_marvin(questions_with_answers=questions_with_answers)
-            _check_answers(results=results, questions_with_answers=questions_with_answers)
+            check_answers(
+                client=client,
+                model=model,
+                questions_with_answers=questions_with_answers,
+            )
 
 
 if __name__ == "__main__":
+    openai_client = OpenAI(
+        api_key="notNeeded", base_url="http://localhost:8001/anjor-1/openai"
+    )
+
     title()
     category, number_of_questions = quiz_params()
-    run_quiz(category=category, number_of_questions=number_of_questions)
+    run_quiz(
+        client=openai_client,
+        model="gpt-3.5-turbo",
+        category=category,
+        number_of_questions=number_of_questions,
+    )
